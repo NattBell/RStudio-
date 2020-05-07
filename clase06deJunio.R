c8f8728e5937337e8dd0ce51af28da90ea0205ee
NatalySM <nataly.silva.munoz@gmail.com>
2020-05-06 20:58:39 -0400

    Clase 06 de Mayo RStudio

---
+++ clase06deJunio.R
@@ -0,0 +1,110 @@
+
+
+
+var <- 1
+if(2<4){
+  var1 <- 2+2
+}
+var1
+
+#####
+
+var1 <- 2
+var1 == 2
+
+
+if(var1 == 2) {
+  var1 <- var1 + 1
+}
+var1
+
+#######
+
+var1 <- 5
+var1
+
+if(var1 %% 2 == 0){
+  var1 <- var1 + 1
+  
+}
+var1  
+
+######
+
+varX <- 5
+print(varX) 
+
+
+if(varX %% 2 == 0){
+  print("El numero es par")
+  VARx <- varX * 2
+} else{
+  print("El numero es impar")
+  varX <- varX * 3
+}
+print(varX)
+
+
+#######
+ejercicio de la ppt
+
+X <- 0 
+
+if(X<=0){
+  print("X es menor igual a cero")
+  X <- 1
+  print(X)
+  
+}else {
+  print("X es mayor a cero")
+  X <- 2*X^2
+  print(X)
+}
+
+
+####
+
+y <- 4
+
+if(y == 3){
+  print("y vale tres")
+} else if( y < 3 ){
+  print("y es menor a tres")
+} else {
+  print("y es mayor a tres")
+}
+
+
+#########  
+X <- -6
+
+if(X>0){
+  print("X es mayor a cero")
+  X <- 2*X^2
+  print(X)
+} else if(X<0){
+  print("X es menor a cero")
+  X <- 1
+  print(X)
+} else{
+  print("X es igual a cero")
+  X <- 5
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
